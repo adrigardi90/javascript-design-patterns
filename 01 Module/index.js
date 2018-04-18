4fd97ee8fc10863ad71f2b274e1058b96bbe25a5
Adrian Garcia <adrigar90@gmail.com>
2018-04-18 22:44:06 +0100

    Module

--- 01 Module/index.js
+++ 01 Module/index.js
@@ -1,13 +1,1 @@
-import * as ReactDOM from 'react-dom';
-import * as React from 'react';
 
-import { RouterComponent } from './router';
-import { configureStore } from './store/store';
-
-export const store = configureStore();
-
-ReactDOM.render(
-    <RouterComponent
-        store={store} />,
-    document.getElementById('root')
-);
